c6d027e0ffb188f2a336078023f21c5c9bbf5a1b
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-09-03 03:45:53 +0430

    I fucked up the working branch

--- javaScript.js
+++ javaScript.js
@@ -14,34 +14,49 @@ const setLineThrough = () => {
 const edit = () => {
     const editClicked = document.getElementsByClassName("noteEdit");
     const notePara = document.getElementsByClassName("note");
-    document.body.onclick = function (evt) {
-        var evt = window.event || evt; //window.event for IE
-        if (!evt.target) {
-            evt.target = evt.srcElement; //extend target property for IE
+    document.body.onclick = function (event) {
+        var selected = event.target;
+        if (selected.nodeName === "I" && (selected.getAttribute("class") === "fa fa-pencil-square-o noteEdit rawEdit" || selected.getAttribute("class") === "fa fa-pencil-square-o noteEdit")) {
+        
+            let attrSelected = Number(selected.getAttribute("attribute"));
+            console.log(attrSelected);
+            const notePara = document.getElementsByClassName("note");
+            let newText = prompt("Please Edit", notePara[(attrSelected + 1)].innerHTML);
+            notePara[attrSelected + 1].innerHTML = newText;
         }
-
-        var selected = evt.target;
-        let attrSelected = selected.getAttribute("attribute");
-        const notePara = document.getElementsByClassName("note");
-        let newText = prompt("Please Edit", notePara[attrSelected].innerHTML);
-        notePara[attrSelected].innerHTML = newText;
     };
 };
 
-const addTask = () => {
-    const noteContainer = document.getElementsByClassName("noteContainer");
-    document.body.onclick = function (evt) {
-        var evt = window.event || evt; //window.event for IE
-        if (!evt.target) {
-            evt.target = evt.srcElement; //extend target property for IE
-        }
 
-        const notesCurrently = 
-        var selected = evt.target;
-        const attrSelected = Number(selected.getAttribute("attribute"));
-        const divSelected = noteContainer[attrSelected];
-        alert(noteContainer.getAttribute);
-    }; 
+const addTask = () => {
+    document.body.onclick = function (event) {
+
+        const selectedAdd = event.target;
+        if (selectedAdd.nodeName === "I" && (selectedAdd.getAttribute("class") === "fa fa-plus addNote")) {
+            event.preventDefault();
+            const addParentPara = selectedAdd.parentNode;
+            let addParent = addParentPara.parentNode;
+            let counter = 0;
+            if (addParent.children.length > 1) {
+                for (let i = 1; i < addParent.children.length; i++) {
+                    if (addParent.children[i].nodeName == "P") counter++;
+                    console.log(counter);
+                }
+                // appending the new paragraph with the checkbox and edit icon and brakes around them
+                addParent.appendChild(document.getElementsByClassName("break")[0].cloneNode(true));
+                addParent.appendChild(document.getElementsByClassName("rawParagraph")[0].cloneNode(true));
+                addParent.appendChild(document.getElementsByClassName("rawEdit")[0].cloneNode(true));
+                addParent.appendChild(document.getElementsByClassName("rawInput")[0].cloneNode(true));
+                addParent.appendChild(document.getElementsByClassName("break")[0].cloneNode(true));
+                // adding the attribute to the added elements
+                addParent = addParentPara.parentNode;
+                console.log(addParent.children[(addParent.children.length - 2)]);
+                addParent.children[(addParent.children.length - 4)].setAttribute("attribute", counter.toString());
+                addParent.children[(addParent.children.length - 3)].setAttribute("attribute", counter.toString());
+            }
+        
+        }
+    };
 };
 
 const makeNewNote = () => {
