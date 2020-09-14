745fcded9630e2ab53b9a24b9cb277291c85e161
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-09-14 14:44:23 +0430

    it is compelete, could use some UI/UX tho

--- javaScript.js
+++ javaScript.js
@@ -70,3 +70,55 @@ const addNewList = () => {
     note[note.length - 1].setAttribute("contenteditable", "true");
     note[note.length - 1].focus();
 };
+const search = () => {
+    const inputValue = document.getElementById("inputSearch").value;
+    const allTitles = document.getElementsByClassName("title");
+    console.log(allTitles);
+    const allTasks = document.getElementsByClassName("note");
+    const pattern = new RegExp('(\\w*' + inputValue + '\\w*)', 'gi');
+    if (inputValue == "" || inputValue == undefined || inputValue == undefined) {
+        for (let i = 0; i < allTitles.length; i++) {
+                allTitles[i].classList.remove("search");
+        }
+        for (let i = 0; i < allTasks.length; i++) {
+                allTasks[i].classList.remove("search");
+        }
+        return;
+    }
+    for (let i = 0; i < allTitles.length; i++) {
+        if (allTitles[i].textContent.match(pattern)) {
+            allTitles[i].classList.add("search");
+        }
+        else {
+            allTitles[i].classList.remove("search");
+        }
+    }
+    for (let i = 0; i < allTasks.length; i++) {
+        if (allTasks[i].textContent.match(pattern)) {
+            allTasks[i].classList.add("search");
+        }
+        else {
+            allTasks[i].classList.remove("search");
+        }
+    }
+};
+const addCategory = () => {
+    const categoryName = document.getElementById("inputCategory").value;
+    if (categoryName == "" || categoryName == undefined || categoryName == null) return;
+    const div = document.createElement("div");
+    div.classList.add("category");
+    const a = document.createElement("a");
+    a.setAttribute("href", `./${categoryName}`);
+    a.textContent = categoryName;
+    div.appendChild(a);
+    document.getElementById("categories").appendChild(div);
+};
+const removeCategory = () => {
+    const categoryName = document.getElementById("inputCategory").value;
+    if (categoryName == "" || categoryName == undefined || categoryName == null) return;
+    const allCategories = document.getElementsByClassName("category");
+    const categoriesContainer = document.getElementById("categories");
+    for (let i = 0; i < allCategories.length; i++) {
+        if (allCategories[i].children[0].textContent == categoryName) categoriesContainer.removeChild(allCategories[i]);
+    }
+};
